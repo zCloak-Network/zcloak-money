6f2c6b828f2069dbd44cf93ffcbe0f8bba29271d
tower1229 <tower1229@foxmail.com>
2025-09-18 10:05:53 +0800

    Add GSAP and @types/node dependencies; update homepage route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Homepage } from "@/components/homepage";
 
 export const Route = createFileRoute("/")({
-  component: () => <>homepage</>,
+  component: () => <Homepage />,
 });
