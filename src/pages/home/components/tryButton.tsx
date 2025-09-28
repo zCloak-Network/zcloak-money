eef6cdbc1365e30fc1b6f505ca56c41a4ce0736c
zhuangyu <zyno1107@163.com>
2025-09-28 22:12:16 +0800

    link

--- src/pages/home/components/tryButton.tsx
+++ src/pages/home/components/tryButton.tsx
@@ -1,6 +1,6 @@
 export default ({ text = 'Try Demo' }: { text?: string }) => {
   return (
-    <a href="https://beta2.zcloak.money" target='_blank'>
+    <a href="https://app.zcloak.money" target='_blank'>
       <div className="cursor-pointer hover:scale-102 md:hover:scale-105 transition-transform duration-300 flex items-center justify-center w-full md:w-36 py-3 bg-white text-black rounded-lg border-[3px] border-black/20">
         {text}
       </div>
