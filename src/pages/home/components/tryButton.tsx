ad9d335c1627dc814ba7896b6fb7dd390b9e507c
zhuangyu <zyno1107@163.com>
2025-10-16 15:58:31 +0800

    text fix

--- src/pages/home/components/tryButton.tsx
+++ src/pages/home/components/tryButton.tsx
@@ -1,4 +1,4 @@
-export default ({ text = 'Try Demo' }: { text?: string }) => {
+export default ({ text = 'Try Beta' }: { text?: string }) => {
   return (
     <a href="https://app.zcloak.money" target='_blank'>
       <div className="cursor-pointer hover:scale-102 md:hover:scale-105 transition-transform duration-300 flex items-center justify-center w-full md:w-36 py-3 bg-white text-black rounded-lg border-[3px] border-black/20">
